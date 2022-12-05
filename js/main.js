4f370e15de5cbc453a2a8690740cd26cbb6f4c11
배지 <ttsbu@naver.com>
2022-12-05 21:07:20 +0900

    이미지 슬라이드

--- js/main.js
+++ js/main.js
@@ -43,8 +43,27 @@ fadeEls.forEach(function (fadeEl, index) {
   });
 });
 
+// new Swiper(선택자, 옵션)
 new Swiper('.notice-line .swiper-container', {
   direction: 'vertical', // 수직 슬라이드
   autoplay: true, // 자동 재생 여부
   loop: true, // 반복 재생 여부
+});
+
+new Swiper('.promotion .swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  autoplay: { delay: 5000 },
+  pagination: {
+    // 페이지 번호 사용 여부
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true, // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: {
+    // 슬라이드 이전/다음 버튼 사용 여부
+    prevEl: '.promotion .swiper-prev', // 이전 버튼 선택자
+    nextEl: '.promotion .swiper-next', // 다음 버튼 선택자
+  },
 });
